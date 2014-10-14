7eb8d56204754d8591a90b516f7b4ade324adc54
thefenry <thefenry@gmail.com>
2014-10-14 13:17:02 -0700

    Basic search
    Using the angular directives i am able to search based on the username
    and return the info we need.

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
+(function() {
 
-(function(){
-  
   var myApp = angular.module('githubViewer', []);
-  
-  var MainController = function($scope, $http){
-   
-   var onUserComplete = function(response){
-     $scope.user = response.data;
-   };
-   
-   var onError = function(reason){
-     $scope.error = "Could not fetch the user";
-   };
-   
-   $http.get("https://api.github.com/users/thefenry")
-   .then(onUserComplete, onError);
-   
-   $scope.message = "Hello, Angular!";
-   
+
+  var MainController = function($scope, $http) {
+
+    var onUserComplete = function(response) {
+      $scope.user = response.data;
+    };
+
+    var onError = function(reason) {
+      $scope.error = "Could not fetch the user";
+    };
+
+    $scope.search = function(username) {
+      $http.get("https://api.github.com/users/" + username)
+        .then(onUserComplete, onError);
+    }
+
+
+    $scope.username = "angular";
+    $scope.message = "Angular Github Viewer!";
+
   };
-  
+
   myApp.controller('MainController', ["$scope", "$http", MainController]);
-  
-  
+
+
 }());
