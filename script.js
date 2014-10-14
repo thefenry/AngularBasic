c527e3cf4884f702d2129e757ce45c7a213d1df4
thefenry <thefenry@gmail.com>
2014-10-14 13:30:24 -0700

    Using the repeater and formatting some of the content
    This gets a list of all the repos of the user and lists them
    appropriately. We use the  ng-repeat as the directive that replaces the
    for loop.

--- script.js
+++ script.js
@@ -6,10 +6,15 @@
 
     var onUserComplete = function(response) {
       $scope.user = response.data;
+      $http.get($scope.user.repos_url).then(onRepos, onError)
     };
+    
+    var onRepos = function (response) {
+      $scope.repos = response.data;
+    }
 
     var onError = function(reason) {
-      $scope.error = "Could not fetch the user";
+      $scope.error = "Could not fetch the data";
     };
 
     $scope.search = function(username) {
