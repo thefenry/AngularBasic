dcb6859fcd93a159fe7835aa1d11e70ca2961dbe
thefenry <thefenry@gmail.com>
2014-10-14 15:44:39 -0700

    Create a github service
    This new service created will move the $http service to it's own file so
    that now i use a custom service that will do the same behavior and clean
    up the code readability a bit

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
   var myApp = angular.module('githubViewer', []);
 
-  var MainController = function($scope, $http, $interval, $log, $anchorScroll, $location) {
+  var MainController = function($scope, github, $interval, $log, $anchorScroll, $location) {
 
-    var onUserComplete = function(response) {
-      $scope.user = response.data;
-      $http.get($scope.user.repos_url).then(onRepos, onError)
+    var onUserComplete = function(data) {
+      $scope.user = data;
+      github.getRepos($scope.user).then(onRepos, onError)
     };
 
     var onRepos = function(response) {
@@ -33,8 +33,7 @@
 
     $scope.search = function(username) {
       $log.info("Searching for " + username);
-      $http.get("https://api.github.com/users/" + username)
-        .then(onUserComplete, onError);
+      github.getUser(username).then(onUserComplete, onError);
       if (countdownInterval) {
         $interval.cancel(countdownInterval);
         $scope.countdown = null;
@@ -48,6 +47,7 @@
     startCountdown();
   };
 
-  myApp.controller('MainController', ["$scope", "$http", "$interval", "$log", "$anchorScroll", "$location", MainController]);
+  myApp.controller('MainController', ["$scope", "github", "$interval", "$log", "$anchorScroll", "$location", MainController]);
+
 
 }());
