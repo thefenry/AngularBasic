bdcd48850b02af19cb2a8e25fbb03fe3e84cdeb0
thefenry <thefenry@gmail.com>
2014-10-14 14:44:00 -0700

    This is the services timeout example
    This will let me display a countdown timer as time ticks away using the
    interval service. I also have been using another service called $http.

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 
   var myApp = angular.module('githubViewer', []);
 
-  var MainController = function($scope, $http) {
+  var MainController = function($scope, $http, $interval) {
 
     var onUserComplete = function(response) {
       $scope.user = response.data;
       $http.get($scope.user.repos_url).then(onRepos, onError)
     };
-    
-    var onRepos = function (response) {
+
+    var onRepos = function(response) {
       $scope.repos = response.data;
     }
 
@@ -17,6 +17,17 @@
       $scope.error = "Could not fetch the data";
     };
 
+    var decrementCountdown = function() {
+      $scope.countdown -= 1;
+      if ($scope.countdown < 1) {
+        $scope.search($scope.username);
+      }
+    }
+    
+    var startCountdown = function () {
+      $interval(decrementCountdown, 1000, $scope.countdown)
+    }
+    
     $scope.search = function(username) {
       $http.get("https://api.github.com/users/" + username)
         .then(onUserComplete, onError);
@@ -26,10 +37,11 @@
     $scope.username = "angular";
     $scope.message = "Angular Github Viewer!";
     $scope.repoSortOrder = "+stargazers_count";
-
+    $scope.countdown = 5;
+    startCountdown();
   };
 
-  myApp.controller('MainController', ["$scope", "$http", MainController]);
+  myApp.controller('MainController', ["$scope", "$http", "$interval", MainController]);
 
 
 }());
